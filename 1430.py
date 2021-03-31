c2e74ba08adb83cb69ceb7b76a2e3c36f46a6f90
ChungRan <riplac0314@gmail.com>
2021-03-31 22:14:42 +0900

    2021 - 03 - 31

--- 1430.py
+++ 1430.py
@@ -1,14 +1,38 @@
-_ = input()
-answer = sorted(set(input().split()))
-_ = input()
-ask = input().split()
-a = 0
-for x in ask:
-    a = 0
-    for y in answer:
-        if x == y:
-            print('1',end=' ')
-            a = 1
-    if(a == 0):
-        print('0',end=' ')
+import sys
+input = sys.stdin.readline
+
+
+def binary_search(target, data):
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if data[mid] == target:
+            return True  # 함수를 끝내버린다.
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return False
+
+
+def solution():
+    _ = int(input())
+    save = sorted(list(map(int, input().split())))
+    _ = int(input())
+    question = list(map(int, input().split()))
+    answer = ""
+    for i in question:
+        if binary_search(i, save):
+            answer += '1 '
+        else:
+            answer += '0 '
+    print(answer)
+
+
+solution()
+
 
